134cdd01994c927bafc2330f7250863259e7e634
katrinnaya <palomaketrin@gmail.com>
2025-03-16 17:45:10 +0300

    Update replication_dag.py

--- dags/replication_dag.py
+++ dags/replication_dag.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta  # Импорт datetime и timedelta
+from datetime import datetime, timedelta  
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from pymongo import MongoClient
